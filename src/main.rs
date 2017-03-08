dc41fe6a44692d845473eb2f536804c3aede5f35
Brian Manning <brian@xaoc.org>
2017-03-07 16:40:47 -0800

    src/main.rs: added comments explaining what the bits are/do

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,29 @@ use std::path::PathBuf;
 
 fn main() {
    //let mut idg_dir_path = PathBuf::from("~/Files/Doom/idgames");
+   // create an empty PathBuf
    let mut idg_dir = PathBuf::new();
+   // call `home_dir()` and act on what it returns
    match env::home_dir() {
       //Some(idg_dir) => println!("{}", idg_dir.display()),
+      // home_dir() returned something
       Some(home_dir) => idg_dir = home_dir,
+      // home_dir() returned nothing
       None => println!("Can't obtain home directory path!"),
    }
 
+   // append more directories on to idg_dir
    idg_dir.push("Files");
    idg_dir.push("Doom");
    idg_dir.push("idgames");
 
-   println!("Updated іdgames/ path: {}", idg_dir.display());
-   // read the contents of idg_dir/
+   // try and read the contents of idg_dir/
    match fs::read_dir(idg_dir) {
+      // nope, can't read for some reason
       Err(why) => println!("! {:?}", why.kind()),
-      Ok(paths) => for path in paths {
-         println!("> {:?}", path.unwrap().path());
+      // yep, can read contents of idg_dir, see what items are there
+      Ok(items) => for item in items {
+         println!("- {:?}", item.unwrap().path());
       }
    }
 }
